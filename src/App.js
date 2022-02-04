0a900c24ff61afad3d673a2e68ceb943e63d433c
Carla Serrano <carlacarly8@gmail.com>
2022-02-04 17:34:26 -0300

    darkMode-terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [darkMode, setDarkMode] = useState(false);
+  
+   const posts = [
+    {
+      title: 'Aprendiendo ReactJS',
+      content: 'Una de las tecnologías con mayor demanda en el mercado',
+    },
+    {
+      title: 'Aprendiendo Angular',
+      content: 'Una de las tecnologías con mayor demanda en el mercado',
+    },
+    {
+      title: 'Aprendiendo VueJS',
+      content: 'Una de las tecnologías con mayor demanda en el mercado',
+    },
+    {
+      title: 'Aprendiendo NextJS',
+      content: 'Una de las tecnologías con mayor demanda en el mercado',
+    },
+    {
+      title: 'Aprendiendo NestJS',
+      content: 'Una de las tecnologías con mayor demanda en el mercado',
+    },
+    {
+      title: 'Aprendiendo Spring Boot',
+      content: 'Una de las tecnologías con mayor demanda en el mercado',
+    },
+  ];
+  
+  return <div className={ darkMode ? 'wrapper-dark' : 'wrapper-light'}>
+
+    <div  >
+    <header className="container">
+      <div className="welcome">
+        <h1>Hola, soy Carla</h1>
+        <p>Bienvenidos a mi blog!</p>
+      </div>
+      <button type="button" 
+      onClick={()=>setDarkMode(!darkMode)}>
+        {darkMode ? 'Light Mode' : 'Dark Mode'}
+      </button>
+    </header>
+
+    <div className="hero">
+      <h2>Lo mejor de la tecnologia</h2>
+      <button className="btn"
+        className={ darkMode ? 'wrapper-dark' : 'wrapper-light'}
+      >LO ENCONTRÁS ACÁ</button>
     </div>
-  );
+
+    <section className="container" className='posts'>
+      {posts.map((post, index) => (
+            <div key={index} className='post-card'>
+              <p className='post-title'>{post.title}</p>
+              <p className='post-content'>{post.content}</p>
+            </div>
+          ))}
+    </section>
+
+    <footer className="container">
+      <p>Hola, soy Carla | Todos los derechos reservados</p>
+    </footer>
+    </div>;
+  </div>;
 }
 
 export default App;
